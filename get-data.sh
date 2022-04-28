073c55d7ddeef9ac0c54c0cbeada412258dd0c4a
Kathir <katahasin@gmail.com>
2022-04-28 15:51:33 +0200

    final push

--- get-data.sh
+++ get-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ccfiles=(
+ccfiles=( #paste the files you want here
   'crawl-data/CC-MAIN-2014-35/segments/1408500800168.29/warc/CC-MAIN-20140820021320-00000-ip-10-180-136-8.ec2.internal.warc.gz'
   'crawl-data/CC-MAIN-2014-35/segments/1408500800168.29/wat/CC-MAIN-20140820021320-00000-ip-10-180-136-8.ec2.internal.warc.wat.gz'
   'crawl-data/CC-MAIN-2014-35/segments/1408500800168.29/wet/CC-MAIN-20140820021320-00000-ip-10-180-136-8.ec2.internal.warc.wet.gz'
